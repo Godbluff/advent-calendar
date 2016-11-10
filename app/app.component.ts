51a555fd7dd43c08bdd7700274a79054e70dc935
Bjørn Arve Lagim <bjornarvelagim@MacBookWIFI.lan>
2016-11-10 15:47:46 +0100

    Initial Commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+        <h1>Angular Calendar Coming Soon...</h1>
+    `
+})
+export class AppComponent { }
